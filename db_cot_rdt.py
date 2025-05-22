ef3567c5bd6c89758255e689695886ced3070448
Chocolife-96 <zhangyijia@sjtu.edu.cn>
2025-05-22 07:06:42 +0000

    0522

--- db_cot_rdt.py
+++ db_cot_rdt.py
@@ -6,11 +6,31 @@ import time
 import re
 import subprocess
 import signal
-
+import sys
+import keyboard
+# sys.path.append(os.path.abspath('/mnt/ssd_data/rdt_project/sdk/rdt/rdt_rpu0519/scripts'))
 # export ARK_API_KEY='3db64cd1-9134-44ea-ba16-2eb83d6e7d6a' # yijia的key，需要时常看这个key是否用完，用完了就换一个免费的
 # export ARK_API_KEY='080e47d2-2b23-4899-a2e9-68f22cef56a3' # weigang的key，需要时常看这个key是否用完，用完了就换一个免费的
 
+# import rclpy
+# from rclpy.node import Node
+# from std_msgs.msg import Bool
 
+# class BoolListener(Node):
+#     def __init__(self):
+#         super().__init__('bool_listener')
+#         self.subscription = self.create_subscription(
+#             Bool,
+#             'my_bool_topic',
+#             self.listener_callback,
+#             10
+#         )
+#         self.should_continue = False  
+
+#     def listener_callback(self, msg):
+#         self.get_logger().info(f'Received: {msg.data}')
+#         self.should_continue = msg.data  # 更新标志位
+        
 def get_vlm_response(img, vlm_prompt):
     client = Ark(
         # 此为默认路径，您可根据业务所在地域进行配置
@@ -45,16 +65,16 @@ def image_encode(image_path):
     return encoded_image
 
 
-def get_figure_dict():
-    def image_encode(image_path):
-        image = cv2.imread(image_path)
-        _, buffer = cv2.imencode('.jpg', image)
-        encoded_image = base64.b64encode(buffer).decode('utf-8')
-        return encoded_image
-    image_path = ['./figure/step0.jpg', './figure/step1.jpg', './figure/step1_2.jpg', './figure/step2.jpg']
+# def get_figure_dict():
+#     def image_encode(image_path):
+#         image = cv2.imread(image_path)
+#         _, buffer = cv2.imencode('.jpg', image)
+#         encoded_image = base64.b64encode(buffer).decode('utf-8')
+#         return encoded_image
+#     image_path = ['./figure/step0.jpg', './figure/step1.jpg', './figure/step1_2.jpg', './figure/step2.jpg']
 
-    dict = {"0": image_encode(image_path[0]), "1": image_encode(image_path[1]), "2": image_encode(image_path[2]), "3": image_encode(image_path[3])}
-    return dict
+#     dict = {"0": image_encode(image_path[0]), "1": image_encode(image_path[1]), "2": image_encode(image_path[2]), "3": image_encode(image_path[3])}
+#     return dict
 
 def split_tasks_camera(vlm_prompt):
     for _ in range(3):
@@ -69,28 +89,18 @@ def split_tasks_figure(encoded_image, vlm_prompt):
     vlm_response = get_vlm_response(encoded_image, vlm_prompt)
     return vlm_response.choices[0].message.content
 
-
-
-
-
-
-
-
-
 def send_task(task):
     print()
     print("@@ send task: ", task)
     if "sandbag" in task:
         # 启动一个新的进程组
         process = subprocess.Popen(
-            ['source', 'xxx.sh'],
-            preexec_fn=os.setsid  # 在一个新的进程组中运行
+            ['bash', '/mnt/ssd_data/rdt_project/sdk/rdt/rdt_rpu0519/inference4onnx_sandbag.sh'],preexec_fn=os.setsid # 在一个新的进程组中运行
         ) # 调用抓沙包的script
     elif "sponge" in task:
         # 启动一个新的进程组
         process = subprocess.Popen(
-            ['source', 'xxx.sh'],
-            preexec_fn=os.setsid  # 在一个新的进程组中运行
+            ['bash', '/mnt/ssd_data/rdt_project/sdk/rdt/rdt_rpu0519/inference4onnx_sponge.sh'],preexec_fn=os.setsid  # 在一个新的进程组中运行
         )  # 调用海绵清桌子的script
     return process
 
@@ -98,8 +108,9 @@ def send_task(task):
 
 def send_tasks(tasks):
     num = 0
-    CAMERA_INTERVAL = 2 # 需要测量一下摄像头保存照片的间隔
+    CAMERA_INTERVAL = 3 # 需要测量一下摄像头保存照片的间隔
     for task in tasks:
+        time_start = time.time()
         robot_process = send_task(task)
         # print(task)
         next_task = False
@@ -108,15 +119,18 @@ def send_tasks(tasks):
             print("========================")
             print("prompt sent to vlm: ", vlm_prompt)
             print("current camera state: ", str(num))
-            encoded_image = image_encode('path2fig')
+            encoded_image = image_encode('/mnt/ssd_data/rdt_project/sdk/rdt/rdt_rpu0519/saveimage/output.jpg')
             num += 1
             vlm_response = get_vlm_response(encoded_image, vlm_prompt)
 
             print("vlm response: ", vlm_response.choices[0].message.content)
-            if vlm_response.choices[0].message.content == "True":
+            time_end = time.time()
+            #rclpy.spin_once(node, timeout_sec=0.1)
+            if vlm_response.choices[0].message.content == "True" or time_end-time_start > 30:
                 next_task = True
                 # 杀掉整个进程组
                 os.killpg(os.getpgid(robot_process.pid), signal.SIGTERM)
+                #robot_process.terminate()
                 time.sleep(3)
             else:
                 next_task = False
@@ -126,9 +140,14 @@ def send_tasks(tasks):
 
 
 def main():
-    vlm_prompt = "Assume you are a robot, the desk in this picture needs to be cleaned. You have two skills, one is to pick something into the box, the other one is to wipe the table with a sponge. Please give me the sub-actions to complete the desk cleaning task. The sub-actions should be in the form of a list."
+    # rclpy.init(args=None)
+    # node = BoolListener()
+    # os.system('export ARK_API_KEY=\'3db64cd1-9134-44ea-ba16-2eb83d6e7d6a\'')
+    vlm_prompt = "You are a robot, the desk in this picture needs to be cleaned. You have two skills, one is to pick sandbag into the box, the other is to wipe the table with a sponge. Please give me the sub-actions to complete the desk cleaning task. Please write each needed sub-action strictly according to the given format: 1. pick sandbag into the box; 2. wipe the table with a sponge. The sub-actions should be in the form of a list."
     # figure_dict = get_figure_dict()
-    response = split_tasks_figure(figure_dict["0"], vlm_prompt)
+    figure_initial = image_encode('/mnt/ssd_data/rdt_project/sdk/rdt/rdt_rpu0519/saveimage/initialization.jpg')
+    response = split_tasks_figure(figure_initial, vlm_prompt)
+    # print(response)
     # 使用正则表达式按数字加点分割
     spilted_response = re.split(r'\d+\.\s*', response)
 
